c569747ae2fc7f33c2faa9ee0505b98e58238415
Sameerkumar Jigneshkumar Barot <W0856698@myscc.ca>
2024-06-20 01:38:09 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpringFurnitures.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
